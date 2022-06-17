d1347e621da55c6acd019987367f4170a8e81ef8
Nihilistzsche <nihilistzsche@gmail.com>
2022-06-17 09:16:50 -0700

    1.1.0

--- data-updates.lua
+++ data-updates.lua
@@ -1,62 +1,94 @@
 local vanillaScienceNames = {"automation", "logistic", "military", "chemical", "production", "utility"}
 local vanillaScience = {}
 for _, vanillaPack in ipairs(vanillaScienceNames) do
-    vanillaScience[vanillaPack .. "-science-pack"] = true
+  vanillaScience[vanillaPack .. "-science-pack"] = true
 end
 
 local infusedSciencePacks = {}
 
+local function is_valid_science_pack(science)
+  if not science then
+    return false
+  end
+  if science.name == "space-science-pack" or string.starts_with(science.name, "se-") then
+    return false
+  end
+  if science.name:sub(-13) == "-science-pack" then
+    return true
+  end
+  if science.name:sub(-10) == "-tech-card" then
+    return true
+  end
+  if science.name:sub(-13) == "_science_item" then
+      return true
+  end
+end
+
 for _, science in pairs(data.raw["tool"]) do
-    if science.name ~= "space-science-pack" and science.name:sub(-13) == "-science-pack" then
-        table.insert(infusedSciencePacks, table.deepcopy(science))
-    end
+  if is_valid_science_pack(science) then
+    table.insert(infusedSciencePacks, table.deepcopy(science))
+  end
 end
 
 local useAltOverlay = settings.startup["is-use-alt-overlay"].value
-
 for i, infusedScience in ipairs(infusedSciencePacks) do
-    local scienceName = infusedScience.name
-    infusedScience.name = "infused-" .. scienceName
-    
-    infusedScience.order = "z[" .. i .. infusedScience.name .. "]"
-    
-    local infusedOverlay = nil
-    if useAltOverlay == "Never" or (useAltOverlay == "Modded only" and vanillaScience[scienceName]) then
-        infusedOverlay = "__infused-science__/graphics/icons/infused.png"
-    elseif useAltOverlay == "Always" or (useAltOverlay == "Modded only" and not vanillaScience[scienceName]) then
-        infusedOverlay = "__infused-science__/graphics/icons/infused_alt.png"
+  local scienceName = infusedScience.name
+  infusedScience.name = "infused-" .. scienceName
+  if infusedScience.localised_name then
+    if type(infusedScience.localised_name) == "table" then
+      infusedScience.localised_name = {"item-name.infused", infusedScience.localised_name}
+    else
+      infusedScience.localised_name = infusedScience.localised_name .. " - Infused"
     end
-    
-    infusedScience.icons = {
-        {icon = infusedScience.icon, icon_size = infusedScience.icon_size},
-        {icon = infusedOverlay, icon_size = 64}
-    }
-    infusedScience.icon = nil
-    infusedScience.icon_size = nil
-    
-    data:extend({infusedScience, {
+  else
+    infusedScience.localised_name = {"item-name.infused", {"item-name." .. scienceName}}
+  end
+  infusedScience.order = "z[" .. i .. infusedScience.name .. "]"
+
+  local infusedOverlay = nil
+  if useAltOverlay == "Never" or (useAltOverlay == "Modded only" and vanillaScience[scienceName]) then
+    infusedOverlay = "__infused-science__/graphics/icons/infused.png"
+  elseif useAltOverlay == "Always" or (useAltOverlay == "Modded only" and not vanillaScience[scienceName]) then
+    infusedOverlay = "__infused-science__/graphics/icons/infused_alt.png"
+  end
+
+  infusedScience.icons = {
+    {icon = infusedScience.icon, icon_size = infusedScience.icon_size},
+    {icon = infusedOverlay, icon_size = 64}
+  }
+  infusedScience.icon = nil
+  infusedScience.icon_size = nil
+
+  data:extend(
+    {
+      infusedScience,
+      {
         type = "recipe",
         category = "crafting-with-fluid",
         name = infusedScience.name,
         enabled = false,
         energy_required = 20,
-        ingredients =
-        {
-            {scienceName, 1},
-            {type="fluid", name="is-infusion-fluid", amount=50}
+        ingredients = {
+          {scienceName, 1},
+          {type = "fluid", name = "is-infusion-fluid", amount = 50}
         },
         result_count = 1,
         result = infusedScience.name
-    }})
+      }
+    }
+  )
 end
 
 if mods["AlienSpaceScience"] then
-    local tech = data.raw["technology"]["space-science-pack"]
-    for i, effect in ipairs(tech.effects) do
-        if effect.type == "unlock-recipe" and effect.recipe == "space-science-pack" then
-            table.remove(tech.effects, i)
-        end
+  local tech = data.raw["technology"]["space-science-pack"]
+  for i, effect in ipairs(tech.effects) do
+    if effect.type == "unlock-recipe" and effect.recipe == "space-science-pack" then
+      table.remove(tech.effects, i)
     end
-    
-    table.insert(data.raw["technology"]["is-infusion-fluid"].effects, {type = "unlock-recipe", recipe = "is-infusion-fluid-artifact"})
+  end
+
+  table.insert(
+    data.raw["technology"]["is-infusion-fluid"].effects,
+    {type = "unlock-recipe", recipe = "is-infusion-fluid-artifact"}
+  )
 end
--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -2,21 +2,68 @@ local function modify_lab_size(size)
     return math.floor((size * 4 / 3) + 0.5)
 end
 
+local base_path = "__base__/graphics/entity/lab/"
+local off_layers = {
+    normal = {
+        {base_path.."lab.png", 98, 87},
+        {base_path.."lab-integration.png", 122, 81},
+        {base_path.."lab-shadow.png", 122, 68}
+    },
+    hr = {
+        {base_path.."hr-lab.png", 194, 174},
+        {base_path.."hr-lab-integration.png", 242, 162},
+        {base_path.."hr-lab-shadow.png", 242, 136}
+    }
+}
+local on_layers = {
+    normal = {
+        {base_path.."lab.png", 98, 87},
+        {base_path.."lab-integration.png", 122, 81},
+        {base_path.."lab-light.png", 106, 100},
+        {base_path.."lab-shadow.png", 122,68}
+    },
+    hr = {
+        {base_path.."hr-lab.png", 194, 174},
+        {base_path.."hr-lab-integration.png", 242, 162},
+        {base_path.."hr-lab-light.png", 216, 192},
+        {base_path.."hr-lab-shadow.png", 242, 136}
+    }
+}
+
 local function modify_lab_layers(layers)
-    for _, layer in ipairs(layers) do
-        layer.filename = layer.filename:gsub("__base__", "__infused-science__")
-        layer.hr_version.filename = layer.hr_version.filename:gsub("__base__", "__infused-science__")
+    local base_layers = #layers == 3 and off_layers or on_layers
+    for i, layer in ipairs(layers) do
+        layer.filename = base_layers.normal[i][1]:gsub("__base__", "__infused-science__")
+        layer.hr_version.filename = base_layers.hr[i][1]:gsub("__base__", "__infused-science__")
     
-        layer.width = modify_lab_size(layer.width)
-        layer.height = modify_lab_size(layer.height)
-        layer.hr_version.width = modify_lab_size(layer.hr_version.width)
-        layer.hr_version.height = modify_lab_size(layer.hr_version.height)
+        layer.width = modify_lab_size(base_layers.normal[i][2])
+        layer.height = modify_lab_size(base_layers.normal[i][3])
+        layer.hr_version.width = modify_lab_size(base_layers.hr[i][2])
+        layer.hr_version.height = modify_lab_size(base_layers.hr[i][3])
+    end
+end
+
+local base_remnant_path = "__base__/graphics/entity/lab/remnants/"
+local base_remnant_layers = {
+    normal = {base_remnant_path.."lab-remnants.png", 134, 100},
+    hr = {base_remnant_path.."hr-lab-remnants.png", 266, 196}
+}
+
+local function modify_lab_remnant_layers(layers)
+    for _, layer in ipairs(layers) do
+        layer.filename = base_remnant_layers.normal[1]:gsub("__base__", "__infused-science__")
+        layer.hr_version.filename = base_remnant_layers.hr[1]:gsub("__base__", "__infused-science__")
+    
+        layer.width = modify_lab_size(base_remnant_layers.normal[2])
+        layer.height = modify_lab_size(base_remnant_layers.normal[3])
+        layer.hr_version.width = modify_lab_size(base_remnant_layers.hr[2])
+        layer.hr_version.height = modify_lab_size(base_remnant_layers.hr[3])
     end
 end
 
 local infusionLab = table.deepcopy(data.raw["lab"]["lab"])
 infusionLab.name = "is-infusion-lab"
-infusionLab.icon = infusionLab.icon:gsub("__base__", "__infused-science__")
+infusionLab.icon = "__infused-science__/graphics/icons/lab.png"
 infusionLab.minable.result = "is-infusion-lab"
 infusionLab.corpse = "is-infusion-lab-remnants"
 infusionLab.energy_usage = "150kW"
@@ -30,10 +77,10 @@ modify_lab_layers(infusionLab.off_animation.layers)
 
 local infusionLabRemnants = table.deepcopy(data.raw["corpse"]["lab-remnants"])
 infusionLabRemnants.name = "is-infusion-lab-remnants"
-infusionLabRemnants.icon = infusionLabRemnants.icon:gsub("__base__", "__infused-science__")
+infusionLabRemnants.icon = "__infused-science__/graphics/icons/lab.png"
 infusionLabRemnants.selection_box = {{-2, -2}, {2, 2}}
 infusionLabRemnants.tile_width = 4
 infusionLabRemnants.tile_height = 4
-modify_lab_layers(infusionLabRemnants.animation)
+modify_lab_remnant_layers(infusionLabRemnants.animation)
 
 data:extend({infusionLab, infusionLabRemnants})
