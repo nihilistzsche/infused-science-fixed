d55f4dfc0bc6b227abde22feba3f2a6ef4857324
Nihilistzsche <nihilistzsche@gmail.com>
2023-12-27 22:26:52 -0800

    SciencePackGalore -> Forked

--- control.lua
+++ control.lua
@@ -1,16 +1,21 @@
-remote.add_interface("infused-science-fixed", function()
-    local tbl = {
-        required_mods = {"infused-science-fixed"},
-        milestones = {
-            {type = "group", name = "Science"}
+remote.add_interface("infused-science-fixed", {
+    milestones_preset_addons = function()
+        local ret = {
+            ["Infused Science (Fixed)"] = {
+                required_mods = { "infused-science-fixed" },
+                milestones = {
+                    { type = "group", name = "Science" },
+                },
+            },
         }
-    } 
-    for _, science in pairs(game.item_prototypes) do
-        if science and science.type == "tool" and science.name:sub(1, 8) == "infused-" then
-            log("Adding milestone for: " .. science.name)
-            table.insert(tbl.milestones, {type = "item", name = science.name, quantity = 1})
-            table.insert(tbl.milestones, {type = "item", name = science.name, quantity = 1000, next = "x10"})
+        local tbl = ret["Infused Science (Fixed)"]
+        for _, science in pairs(game.item_prototypes) do
+            if science and science.type == "tool" and science.name:sub(1, 8) == "infused-" then
+                log("Adding milestone for: " .. science.name)
+                table.insert(tbl.milestones, { type = "item", name = science.name, quantity = 1 })
+                table.insert(tbl.milestones, { type = "item", name = science.name, quantity = 1000, next = "x10" })
+            end
         end
-    end
-    return tbl
-end)
+        return ret
+    end,
+})
